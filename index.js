14c8005dfaa420d3ba7baa488db8f89cad764923
btoner <ben.toner@wework.com>
2018-10-10 19:45:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+  fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json()).then(renderBooks)
+
 }
 
 function renderBooks(json) {
